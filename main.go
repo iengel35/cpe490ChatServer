10c4224086c68a45dfb4ebf425ca359287ac052b
Izy Engel <isabelle.engel@gmail.com>
2020-12-16 20:26:52 -0500

    added automated server messages

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	static "github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
+	"fmt"
+	"rsc.io/quote"                                                                                                                                                                                       
+	"strings"
 )
+
 
 func main() {
 	r := gin.Default()
@@ -15,10 +19,36 @@ func main() {
 	r.GET("/ws", func(c *gin.Context) {
 		m.HandleRequest(c.Writer, c.Request)
 	})
+ 
 
+	//messages format: {"username":"Iz","content":"hi"}
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		
+		fmt.Println(string(msg))
+
+		stringMsg:= string(msg)
+		stringMsg = strings.ToLower(stringMsg)
+		if(strings.Contains(stringMsg, "hi") || strings.Contains(stringMsg, "hello") || strings.Contains(stringMsg, "hey") || strings.Contains(stringMsg, "howdy")) {
+			welcomeMsg  := []byte("{\"username\":\"Host\",\"content\":\"" + quote.Hello() + "\"}")
+			m.Broadcast(welcomeMsg)
+		}
+		 if(strings.Contains(stringMsg, "proverb") || strings.Contains(stringMsg, "advice") || strings.Contains(stringMsg, "wisdom")) {
+			welcomeMsg  := []byte("{\"username\":\"Host\",\"content\":\"" + quote.Go() + "\"}")
+			m.Broadcast(welcomeMsg)
+		}
+		 if(strings.Contains(stringMsg, "glass")) {
+			welcomeMsg  := []byte("{\"username\":\"Host\",\"content\":\"" + quote.Glass() + "\"}")
+			m.Broadcast(welcomeMsg)
+		}
+		 if(strings.Contains(stringMsg, "optimize") || strings.Contains(stringMsg, "optimization") ) {
+			welcomeMsg  := []byte("{\"username\":\"Host\",\"content\":\"" + quote.Opt() + "\"}")
+			m.Broadcast(welcomeMsg)
+		}
+		
 		m.Broadcast(msg)
 	})
-
+	
 	r.Run(":5000")
 }
+
+
